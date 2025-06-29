42227c1540cac28c3a9b07ef35c8006b5c73b741
gaki2 <qudrkr0815@gmail.com>
2025-06-29 15:48:54 +0900

    chore: render SlackChannelSelect

--- apps/web/src/app/main/leftPanel/slack/SlackSettingView.tsx
+++ apps/web/src/app/main/leftPanel/slack/SlackSettingView.tsx
@@ -56,7 +56,7 @@ export const SlackSettingView = () => {
 
   return (
     <div className='flex flex-col gap-4 px-4 grow'>
-      {/*<SlackChannelSelect />*/}
+      <SlackChannelSelect />
       <p className='text-lg'>Settings</p>
       <div className='flex w-full items-center justify-between rounded-lg border p-3 shadow-xs gap-2'>
         <div className='flex flex-col items-start gap-2'>
