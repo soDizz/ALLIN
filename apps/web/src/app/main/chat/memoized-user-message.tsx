dbd769034b924aebf42461717447613b96feaf40
gaki2 <qudrkr0815@gmail.com>
2025-06-29 20:35:52 +0900

    fix: update UserMessage UI

--- apps/web/src/app/main/chat/memoized-user-message.tsx
+++ apps/web/src/app/main/chat/memoized-user-message.tsx
@@ -4,7 +4,7 @@ import { memo } from 'react';
 export const MemoizedUserMessage = memo(({ content }: { content: string }) => {
   return (
     <div className='not-prose flex flex-col my-4 ml-auto max-w-[80%]'>
-      <Badge className='text-sm p-2 rounded-xl text-wrap whitespace-pre-wrap break-keep'>
+      <Badge className='text-sm p-1.5 px-2 rounded-xl text-wrap whitespace-pre-wrap break-keep'>
         {content}
       </Badge>
     </div>
