c3b1a5aa85b2841da0339aa7446e0bfc3c2344ae
gaki2 <qudrkr0815@gmail.com>
2025-09-30 09:36:01 +0900

    feat: Chat 모듈을 관리하는 ChatManager 생성

---
+++ apps/web/src/core/ChatManager.ts
@@ -0,0 +1,44 @@
+import type { UIMessage } from '@ai-sdk/react';
+import type { ChatInit } from 'ai';
+import { Chat } from './Chat';
+import { ChatService } from './ChatService';
+import { ChatSummarizer } from './ChatSummarizer';
+import { UIMessageStore } from './UiMessageStore';
+
+export class ChatManager {
+  private static instance: ChatManager;
+  private static chats: Map<string, Chat<UIMessage>> = new Map();
+
+  private constructor() {}
+
+  public static getInstance(): ChatManager {
+    if (!ChatManager.instance) {
+      ChatManager.instance = new ChatManager();
+    }
+    return ChatManager.instance;
+  }
+
+  public static new<UI_MESSAGE extends UIMessage>({ id }: { id: string }) {
+    const summarizer = new ChatSummarizer({
+      api: process.env.SUMMARIZER_API_URL ?? '',
+    });
+    const chatService = new ChatService<UI_MESSAGE>({
+      id,
+      experimental_throttle: 50,
+    });
+    const uiMessageStore = new UIMessageStore<UI_MESSAGE>();
+    return new Chat<UI_MESSAGE>({
+      id,
+      chatService,
+      summarizer,
+      uiMessageStore,
+    });
+  }
+
+  public static getChatById(id: string) {
+    if (ChatManager.chats.has(id)) {
+      return ChatManager.chats.get(id);
+    }
+    return null;
+  }
+}
