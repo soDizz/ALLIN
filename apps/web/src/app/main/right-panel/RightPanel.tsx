42120eeeef12deab1d0b39361923cb2c22c9b0ac
gaki2 <qudrkr0815@gmail.com>
2025-10-01 22:05:09 +0900

    fix: 빌드에러 수정

--- apps/web/src/app/main/right-panel/RightPanel.tsx
+++ apps/web/src/app/main/right-panel/RightPanel.tsx
@@ -3,7 +3,7 @@ import { MiniThread } from './MiniThread';
 export const RightPanel = () => {
   return (
     <div className='p-4 w-[180px] h-full rounded-md border flex flex-col shrink-0 bg-yellow-100'>
-      <MiniThread thread={[]} />
+      {/* <MiniThread thread={[]} /> */}
     </div>
   );
 };
