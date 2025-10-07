0b218609aa31c4b26dc02bb3f2db81fc7c3eccec
gaki2 <qudrkr0815@gmail.com>
2025-10-07 15:42:18 +0900

    feat: 빌드 환경에 맞게 API baseURL 세팅

---
+++ apps/web/src/app/main/initializer/ApiInitializer.tsx
@@ -0,0 +1,18 @@
+import {
+  ALLIN_API_BASE_URL,
+  ALLIN_API_BASE_URL_LOCAL,
+  ApiAgent,
+} from '@allin/api-client';
+import { useLayoutEffect } from 'react';
+
+export const ApiInitializer = () => {
+  useLayoutEffect(() => {
+    ApiAgent.getInstance().setBaseUrl(
+      process.env.NODE_ENV === 'development'
+        ? ALLIN_API_BASE_URL_LOCAL
+        : ALLIN_API_BASE_URL,
+    );
+  }, []);
+
+  return null;
+};
--- apps/web/src/app/main/initializer/Initializer.tsx
+++ apps/web/src/app/main/initializer/Initializer.tsx
@@ -1,9 +1,11 @@
+import { ApiInitializer } from './ApiInitializer';
 import { HotkeyHandler } from './HotkeyHandler';
 import { LocalStorageSync } from './LocalStorageSync';
 
 export const Initializer = () => {
   return (
     <>
+      <ApiInitializer />
       <LocalStorageSync />
       <HotkeyHandler />
     </>
