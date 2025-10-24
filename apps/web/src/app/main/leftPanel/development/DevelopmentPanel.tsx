3674fa86624bfd874e45d9ed938bff09331280d6
gaki2 <qudrkr0815@gmail.com>
2025-10-24 09:04:00 +0900

    chore: devMode

--- apps/web/src/app/main/leftPanel/development/DevelopmentPanel.tsx
+++ apps/web/src/app/main/leftPanel/development/DevelopmentPanel.tsx
@@ -1,22 +1,21 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion';
+import { DB, DB_STORE } from '@/app/idb/db';
 import { Button } from '@/components/ui/button';
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemTitle,
-} from '@/components/ui/item';
 
 export const DevelopmentPanel = () => {
   return (
     <div className='flex flex-col gap-4 px-4'>
-      <li className='list-none flex flex-col gap-2'></li>
+      <li className='list-none flex flex-col gap-2'>
+        <Button
+          className='w-1/2'
+          variant='destructive'
+          onClick={() => {
+            DB.clearStore(DB_STORE.CHANNELS);
+            DB.clearStore(DB_STORE.MESSAGES);
+          }}
+        >
+          Delete Indexed DB
+        </Button>
+      </li>
     </div>
   );
 };
